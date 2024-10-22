d0dc40e82f4f5c62b1e64369a53e99c5a7e63c41
majharul-islam-kobir <majhar13579@gmail.com>
2024-10-22 12:49:02 +0600

    secend commit

--- src/FoodCard/FoodCard.jsx
+++ src/FoodCard/FoodCard.jsx
@@ -12,12 +12,12 @@ function FoodCard(props) {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-red-100">
 
-        <div className=" w-4/12  bg-red-200 p-4 shadow-orange-50 rounded">
+        <div className=" md:w-4/12 sm:w-6/12   bg-red-200 p-4 shadow-orange-50 rounded">
         <img src={image} alt="" />
         <h2 className="mt-4 p-5 text-2xl">{name}</h2>
         <div className="flex justify-between items-center">
           {props.dataIndex == 9 ? null : <button className="bg-emerald-400 ms-6 p-2 px-3 rounded-md tcxt-white" onClick={() =>  props.onClick ()}>Next</button>}
-            {props.dataIndex == 0 ? null : <button className="bg-emerald-400 me-6 p-2 px-3 rounded-md tcxt-white" onClick={() =>  props.test ()}>previous</button>}
+          {props.dataIndex == 0 ? null : <button className="bg-emerald-400 me-6 p-2 px-3 rounded-md tcxt-white" onClick={() =>  props.test ()}>previous</button>}
 
         </div>
         </div>
